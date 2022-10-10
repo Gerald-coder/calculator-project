89a7ef4815f1482f990363b4540a6428933f3575
Oduigwe <paulsongerald00@gmail.com>
2022-10-10 01:38:59 +0100

    added a calculator project

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello index");
+let outputScreen = document.getElementById("output-screen");
+
+function display(num) {
+  outputScreen.value += num;
+}
+
+function calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (err) {
+    alert("invalid");
+  }
+}
+
+function remove() {
+  outputScreen.value = "";
+}
+
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
